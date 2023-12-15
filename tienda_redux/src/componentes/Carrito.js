0e67675fc3eae945adfb043d4848aa3eb179dd69
Magali Victoria Fernandez <magalivictoria85068@gmail.com>
2023-12-14 21:54:27 -0300

    14-12

--- tienda_redux/src/componentes/Carrito.js
+++ tienda_redux/src/componentes/Carrito.js
@@ -4,30 +4,35 @@ import { connect } from "react-redux";
 import { calcularTotalCarrito } from './../reducers/tiendaReducer';
 
 
-const Carrito =({carrito, totalCarrito})=>{
-    return(
-        <div >
+const Carrito = ({ carrito, totalCarrito }) => {
+  return (
+    <div >
+        <div className="contenedor-carrito" >
         <h1 className="texto-centro">Carrito</h1>
-        {carrito.length >0 ? 
-        carrito.map((producto, index)=>{
-          return(
-            <div className="carrito-producto"key={index}>
-            <p className="carrito-p">{producto.nombre}</p>
-            Cantidad {producto.cantidad}
-            </div>
-            
-          );
-        })
-        : <p className="texto-centro">No hay productos</p>
+        {carrito.length > 0 ?
+          carrito.map((producto, index) => {
+            return (
+              <div className="carrito-producto" key={index}>
+                <p className="carrito-p">{producto.nombre}</p>
+                Cantidad {producto.cantidad}
+              </div>
+
+            );
+          })
+          : <p className="texto-centro">No hay productos</p>
         }
+      </div>
+      <footer className="footer-carrito">
         <p className="texto-centro">Total en el carrito: {totalCarrito}</p>
-        </div>
-    );
+      </footer>
+
+    </div>
+  );
 }
-const mapStateToProps = (estado)=>{
+const mapStateToProps = (estado) => {
   return {
-      carrito: estado.carrito,
-      totalCarrito: calcularTotalCarrito(estado.carrito)
+    carrito: estado.carrito,
+    totalCarrito: calcularTotalCarrito(estado.carrito)
   }
 }
 export default connect(mapStateToProps)(Carrito);
